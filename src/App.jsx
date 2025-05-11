639503add0740aeefb86abf1aea5e76ed3c344e8
6bb6md6issc <kevin9966hu@gmail.com>
2025-05-11 12:11:02 -0500

    updated routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
 import Layout from './layout/Layout'
+import ReviewList from './component/ReviewList';
+import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage'
 function App() {
 
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="review" element={<ReviewList/>}/>
+      </Route>
+    </Routes>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Layout from './layout/Layout.jsx'
-import ReviewList from './component/ReviewList.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter} from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      { path: '/', element: <App /> }, 
-      { path: '/review', element: <ReviewList /> }
-    ]
-  }
-]);
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-      <RouterProvider router={router} />
-  </StrictMode>,
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
