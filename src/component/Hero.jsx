c55c2f228763e55cb40e01aee7a8b571d2e107d1
6bb6md6issc <kevin9966hu@gmail.com>
2025-05-10 20:17:04 -0500

    resized the input box

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -11,10 +11,10 @@ const Hero = () => {
 
         <div className='relative h-[570px]'>
             {/* Main Heading */}
-            <div className="text-white underline text-6xl font-bold pl-20 pt-54 lg:text-7xl">
+            <div className="text-white underline text-6xl font-bold ml-6 md:pl-14 lg:pl-20 pt-54 lg:text-7xl">
                 Find your <br /> residential hall
             </div>
-            <div className="w-full max-w-90 h-16 mt-10 ml-20  lg:max-w-120">
+            <div className="w-full max-w-90 h-16 mt-10 ml-6 md:pl-14 lg:ml-18 lg:max-w-120">
                 <input
                     type="text"
                     placeholder="Enter residential hall name"
@@ -23,6 +23,7 @@ const Hero = () => {
                     className="rounded-2xl w-full h-full px-4 py-3 border-2 border-black text-gray-500 bg-white text-lg focus:outline-none focus:border-black"
                 />
             </div>
+
             <div 
                 className="absolute inset-0 bg-cover bg-top h-[570px] -z-10" 
                 style={{ backgroundImage: "url('./usu.png')" }}
