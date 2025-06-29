758cf197b3ae2b4291220d058e444a8c609b746b
6bb6md6issc <kevin9966hu@gmail.com>
2025-06-28 23:54:43 -0700

    fix: added axios interceptor and updated protectedRoutes.jsx to clear expired token

--- src/config/axiosConfig.jsx
+++ src/config/axiosConfig.jsx
@@ -1,18 +1,42 @@
 import axios from 'axios';
-
+import { jwtDecode } from 'jwt-decode';
 axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 
-axios.interceptors.response.use(
-  response => response,
-  error => {
-    if (
-      error.response &&
-      error.response.data.toLowerCase().includes('jwt expired')
-    ) {
-      // Token expired or invalid
+// axios.interceptors.response.use(
+//   response => response,
+//   error => {
+//     if (
+//       error.response &&
+//       error.response.data.toLowerCase().includes('jwt expired')
+//     ) {
+//       // Token expired or invalid
+//       localStorage.removeItem('token');
+//       window.location.href = '/login';
+//     }
+//     return Promise.reject(error);
+//   }
+// );
+
+axios.interceptors.request.use(async (config) => {
+  const token = localStorage.getItem('token');
+
+  if (token) {
+    try {
+      const { exp } = jwtDecode(token);
+      const isExpired = exp * 1000 < Date.now();
+      if (isExpired) {
+        console.log("debugger 1:", config)
+        localStorage.removeItem('token');
+        delete config.headers.Authorization;
+        // Don't attach the token
+        return config;
+      }
+      // Otherwise token is valid, attach it
+    } catch (err) {
+      console.error('Invalid token:', err);
       localStorage.removeItem('token');
-      window.location.href = '/login';
     }
-    return Promise.reject(error);
   }
-);
+
+  return config;
+}, (error) => Promise.reject(error));
--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,11 +1,21 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../provider/authProvider";
+import { jwtDecode } from "jwt-decode";
 
 export const ProtectedRoute = () => {
   const { token } = useAuth();
 
+  const isTokenExpired = (token) => {
+    try {
+      const { exp } = jwtDecode(token);
+      return exp * 1000 < Date.now(); // `exp` is in seconds
+    } catch (err) {
+      return true; // Consider invalid token as expired
+    }
+  };
+
   // Check if the user is authenticated
-  if (!token) {
+  if (!token || isTokenExpired(token)) {
     // If not authenticated, redirect to the login page
     return <Navigate to="/login" replace/>;
   }
