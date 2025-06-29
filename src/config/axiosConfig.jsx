ec89fce42c6cf4ed0855bb7f604bbf80155937ff
6bb6md6issc <kevin9966hu@gmail.com>
2025-06-29 12:08:49 -0700

    chore

--- src/config/axiosConfig.jsx
+++ src/config/axiosConfig.jsx
@@ -12,9 +12,15 @@ axios.interceptors.request.use(async (config) => {
       const { exp } = jwtDecode(token);
       const isExpired = exp * 1000 <= Date.now();
       if (isExpired) {
+        // console.log("before localStorage: ", localStorage.getItem('token'));
+        // console.log("before config: ", config.headers.Authorization);
+        // console.log("before axios.defaults.headers", axios.defaults.headers)
         localStorage.removeItem('token');
         delete config.headers.Authorization;
         delete axios.defaults.headers.common["Authorization"];
+        // console.log("after localStorage: ", localStorage.getItem('token'));
+        // console.log("after config: ", config.headers.Authorization);
+        // console.log("after axios.defaults.headers", axios.defaults.headers)
         // Don't attach the token
       } else{
         config.headers.Authorization = `Bearer ${token}`;
