7b96ec5da74cad246c8ae2590fa823b22c0cbc27
6bb6md6issc <kevin9966hu@gmail.com>
2025-06-30 16:11:11 -0700

    feat: added rating pagination in hall review page

--- src/pages/HallReviewPage.jsx
+++ src/pages/HallReviewPage.jsx
@@ -1,19 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import ReviewList from '../component/review/ReviewList'
 import ReviewHeader from '../component/review/ReviewHeader';
-import { useParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
 import axios from 'axios';
 import NoSoBuilding from '../component/review/NoSoBuilding';
-
+import PaginationBar from '../component/PaginationBar';
 
 const HallReviewPage = () => {
   const { buildingId } = useParams()
   const [data, setData] = useState({})
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentPage = parseInt(searchParams.get("page")) || 1;
   
   useEffect(() => {
     const fetchReview = async () => {
       try{
-        const response = await axios.get(`/api/v1/rating/${buildingId}`);
+        const response = await axios.get(`/api/v1/rating/${buildingId}?page=${currentPage-1}`);
         setData(response.data);
       } catch(error) {
         console.log("Error occurred" + error);
@@ -21,15 +24,18 @@ const HallReviewPage = () => {
      }
     };
     fetchReview();
-  }, [buildingId])
+  }, [buildingId, currentPage])
 
   if (data === null) return <NoSoBuilding />
 
   return (
-    <div className='mb-20'>
-      <ReviewHeader buildingName={data.building_name} buildingId={data.building_id}/>
-      <ReviewList reviews={data.ratings || []}/>
-    </div>
+    <>
+      <div className='mb-20'>
+        <ReviewHeader buildingName={data.building_name} buildingId={data.building_id}/>
+        <ReviewList reviews={data.ratings || []}/>
+      </div>
+      <PaginationBar currentPage={currentPage} pageCount={data.total_pages} />
+    </>
   )
 }
 
